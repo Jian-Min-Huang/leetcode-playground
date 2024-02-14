88895ba52d45a6c5bc22b4c9bcabec587d7b2f70
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-02-15 02:53:20 +0800

    :art: Refactor LinkedListTests to include assertions
    The LinkedListTests class has been enhanced with more extensive testing. The code now includes a variety of assertions to verify the behavior of LinkedList operations more strictly. Along with this, added new test methods for Stack, Queue, and Deque.

--- src/test/java/LinkedListTests.java
+++ src/test/java/LinkedListTests.java
@@ -1,12 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 
 public class LinkedListTests {
-    @Test
-    void name() {
 
-        // [100, 101, 102, 103, 104]
+    @Test
+    void testList() {
+        // Init
         final LinkedList<Integer> elements = new LinkedList<>() {{
             add(100);
             add(101);
@@ -15,23 +16,54 @@ public class LinkedListTests {
             add(104);
         }};
 
-        elements.add(105);
-        elements.addLast(106);
-        elements.addFirst(99);
-        elements.add(1, 99);
+        // Create
+        // [100, 101, 102, 103, 104]
+        elements.add(105); // [100, 101, 102, 103, 104, 105]
+        Assertions.assertEquals(105, elements.getLast());
+        elements.addLast(106); // [100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(106, elements.getLast());
+        elements.addFirst(98); // [98, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(98, elements.getFirst());
+        elements.add(1, 99); // [98, 99, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(99, elements.get(1));
 
-        elements.get(1);
-        elements.getFirst();
-        elements.getLast();
-        elements.indexOf(99);
-        elements.lastIndexOf(99);
+        // Read
+        // [98, 99, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(99, elements.get(1));
+        Assertions.assertEquals(98, elements.getFirst());
+        Assertions.assertEquals(106, elements.getLast());
+        Assertions.assertEquals(1, elements.indexOf(99));
+        elements.add(1, 98); // [98, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(1, elements.lastIndexOf(98));
 
-        elements.set(2, 101);
+        // Update
+        // [98, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+        elements.set(0, 97); // [97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(97, elements.get(0));
 
-        elements.remove();
-        elements.removeFirst();
-        elements.removeLast();
-        elements.remove(3);
-        elements.removeFirstOccurrence(102);
+        // Delete
+        // [97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+        elements.remove(); // [98, 99, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(98, elements.getFirst());
+        elements.removeFirst(); // [99, 100, 101, 102, 103, 104, 105, 106]
+        Assertions.assertEquals(99, elements.getFirst());
+        elements.removeLast(); // [99, 100, 101, 102, 103, 104, 105]
+        Assertions.assertEquals(105, elements.getLast());
+        elements.remove(3); // [99, 100, 101, 103, 104, 105]
+        Assertions.assertEquals(103, elements.get(3));
+        elements.removeFirstOccurrence(100);
+        Assertions.assertEquals(101, elements.get(1));
+    }
+
+    @Test
+    void testStack() {
+    }
+
+    @Test
+    void testQueue() {
+    }
+
+    @Test
+    void testDeque() {
     }
 }
